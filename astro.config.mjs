7bfd1dfe60e1fde8103988d7cf53ba20e5da7dae
Joe Hsu <jhsu.x1@gmail.com>
2026-07-31 20:44:09 -0400

    update home design

--- astro.config.mjs
+++ astro.config.mjs
@@ -29,7 +29,7 @@ export default defineConfig({
 
   vite: {
     server: {
-      allowedHosts: ["localhost", "bunarch.local"]
+      allowedHosts: ["localhost", "bunarch.tail3428.ts.net", "atjhsu.localhost"]
     }
   },
 
